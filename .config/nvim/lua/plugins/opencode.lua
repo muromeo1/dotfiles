1be6bd0dbb27fc24832afd23ad5a9d36f6f64bf6
Murilo Romeo <muriloh65@gmail.com>
2026-04-15 09:00:35 -0300

    fix: fixes shift-enter on opencode

--- .config/nvim/lua/plugins/opencode.lua
+++ .config/nvim/lua/plugins/opencode.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("opencode").setup({
       preferred_picker = "fzf",
-      default_mode = "sisyphus",
+      default_mode = "Sisyphus (Ultraworker)",
       keymap = {
         input_window = {
           ["<tab>"] = { "switch_mode", mode = { "n", "i" }, desc = "Switch between modes (build/plan)" },
